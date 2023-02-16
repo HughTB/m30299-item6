673f98df1bc9f72e294f936f00aa2d74ad8577ce
HughTB <hugh.baldwin71@gmail.com>
2023-02-16 23:32:20 +0000

    add configuration challenge and document classes and functions

--- backend.py
+++ backend.py
@@ -1,84 +1,108 @@
 class SmartDevice():
+    """Base class for all smart devices, contains methods and properties that all devices must have"""
     def __init__(self):
+        """Constructor, sets switchedOn property to false by default"""
         self.switchedOn = False
 
     def toggleSwitch(self):
+        """Toggles the property switchedOn between true and false"""
         self.switchedOn = not self.switchedOn
     
     def getSwitchedOn(self):
+        """Returns the value of switchedOn"""
         return self.switchedOn
 
 class SmartPlug(SmartDevice):
+    """SmartPlug, inherits from SmartDevice. Contains methods and properties that a smart plug may need to function"""
     def __init__(self):
+        """Constructor, sets consumptionRate to 0 by default"""
         super().__init__()
         self.consumptionRate = 0
 
     def __str__(self):
+        """Returns a string containing information about the smart plug"""
         text = "Plug - "
         text += "on, " if self.switchedOn else "off, "
         text += f"consumption rate: {self.consumptionRate}"
         return text
     
     def getConsumptionRate(self):
+        """Returns the value of consumptionRate"""
         return self.consumptionRate
     
     def setConsumptionRate(self, newConsumptionRate: int):
+        """Sets the value of consumptionRate, ensuring it is in the range 0-150"""
         if newConsumptionRate >= 0 and newConsumptionRate <= 150:
             self.consumptionRate = newConsumptionRate
 
 washModes = ["Daily wash", "Quick wash", "Eco"]
 
 class SmartWashingMachine(SmartDevice):
+    """SmartWashingMachine, inhertis from SmartDevice. Contains methods and properties that a smart washing machine may need to function"""
     def __init__(self):
+        """Constructor, sets washMode to "Daily wash" by default"""
         super().__init__()
         self.washMode = "Daily wash"
 
     def __str__(self):
+        """Returns a string containing information about the smart washing machine"""
         text = "Washing Machine - "
         text += "on, " if self.switchedOn else "off, "
         text += f"wash mode: {self.washMode}"
         return text
     
     def getWashMode(self):
+        """Returns the value of washMode"""
         return self.washMode
     
     def setWashMode(self, newWashMode):
+        """Sets the value of washMode, ensuring it is a valid wash mode, as specified in washModes"""
         if newWashMode in washModes:
             self.washMode = newWashMode
 
 class SmartHome():
+    """SmartHome, contains methods and properties that a smart home controller may need to function"""
     def __init__(self):
+        """Constructor, sets devices to a new, empty array"""
         self.devices = []
 
     def __str__(self):
+        """Returns a string containing information about all of the smart devices contained in the devices array"""
         text = "Smart Home - Devices:"
         for device in self.devices:
             text += "\n" + str(device)
         return text
 
     def getDevices(self):
+        """Returns an array of devices in the smart home"""
         return self.devices
     
     def getDeviceAt(self, index: int):
+        """Returns the device at the specified index in the array devices"""
         return self.devices[index]
     
     def addDevice(self, device: SmartDevice):
+        """Appends a new device to the list of smart devices"""
         self.devices.append(device)
 
     def toggleSwitch(self, index):
+        """Toggles the switch of the device at the specified index in the list of smart devices"""
         self.devices[index].toggleSwitch()
 
     def turnOnAll(self):
+        """Turns on all smart devices in the device list"""
         for device in self.devices:
             if not device.getSwitchedOn():
                 device.toggleSwitch()
     
     def turnOffAll(self):
+        """Turns off all smart devices in the device list"""
         for device in self.devices:
             if device.getSwitchedOn():
                 device.toggleSwitch()
 
 def testSmartPlug():
+    """Tests the functionality of the SmartPlug class"""
     plug = SmartPlug()
 
     plug.toggleSwitch()
@@ -89,6 +113,7 @@ def testSmartPlug():
     print(plug)
 
 def testSmartWashingMachine():
+    """Tests the functionality of the SmartWashingMachine class"""
     washingMachine = SmartWashingMachine()
 
     washingMachine.toggleSwitch()
@@ -99,6 +124,7 @@ def testSmartWashingMachine():
     print(washingMachine)
 
 def testSmartHome():
+    """Tests the functionality of the SmartHome class"""
     smartHome = SmartHome()
 
     plug1 = SmartPlug()
--- frontend.py
+++ frontend.py
@@ -8,6 +8,7 @@ turnedOnDevices = 0
 lblCount = Label(mainWin, text=f"{turnedOnDevices} devices turned on")
 
 def setupHome():
+    """Creates and adds devices to the smartHome object"""
     smartHome.addDevice(SmartWashingMachine())
     smartHome.addDevice(SmartPlug())
     smartHome.addDevice(SmartWashingMachine())
@@ -15,14 +16,52 @@ def setupHome():
     smartHome.addDevice(SmartWashingMachine())
 
 def setupWindow():
-    mainWin.geometry("480x112")
+    """Sets the size, title and column sizes of the main window"""
+    mainWin.geometry("550x112")
     mainWin.resizable(False, False)
     mainWin.title("Smart Home Controller - up2157117")
     mainWin.grid_columnconfigure(0, weight=2)
     mainWin.grid_columnconfigure(1, weight=1)
     mainWin.grid_columnconfigure(2, weight=1)
 
+def configureDevice(device: SmartDevice):
+    """Opens a new window containing configuration settings for a SmartDevice object"""
+    configWin = Toplevel()
+    configWin.geometry("250x50")
+    configWin.resizable(False, False)
+    configWin.title("Configure")
+    configWin.grid_columnconfigure(0, weight=1)
+    configWin.grid_columnconfigure(1, weight=1)
+
+    entOption = Entry(configWin, width=15)
+    entOption.grid(row=0, column=1, sticky="w")
+    lblOption = Label(configWin, anchor="e", width=15)
+    lblOption.grid(row=0, column=0, sticky="e")
+
+    if isinstance(device, SmartPlug):
+        entOption.insert(0, device.getConsumptionRate())
+        lblOption.configure(text="Consumption Rate: ")
+    elif isinstance(device, SmartWashingMachine):
+        entOption.insert(0, device.getWashMode())
+        lblOption.configure(text="Wash Mode: ")
+
+    def submit():
+        if isinstance(device, SmartPlug):
+            device.setConsumptionRate(int(entOption.get()))
+        elif isinstance(device, SmartWashingMachine):
+            device.setWashMode(entOption.get())
+
+        updateWidgets()
+        configWin.destroy()
+
+    btnCancel = Button(configWin, text="Cancel", command=configWin.destroy)
+    btnCancel.grid(row=1, column=0)
+
+    btnSubmit = Button(configWin, text="Submit", command=submit)
+    btnSubmit.grid(row=1, column=1)
+
 def updateWidgets():
+    """Updates the list of devices shown in the main window"""
     turnedOnDevices = 0
 
     for i in range(0, len(deviceTexts)):
@@ -34,6 +73,7 @@ def updateWidgets():
     lblCount.configure(text=f"{turnedOnDevices} devices turned on")
 
 def setupWidgets():
+    """Creates the widgets for all devices in the smartHome object"""
     lblTitle = Label(mainWin, text="Smart Home", font=("Segoe UI", 18))
     lblTitle.grid(row=0, column=0, columnspan=2)
 
@@ -52,11 +92,15 @@ def setupWidgets():
     btnAllOn.grid(row=2, column=0, padx=2, sticky="w")
 
     numOfDevices = len(smartHome.getDevices())
-    mainWin.geometry(f"480x{112 + (26 * numOfDevices)}")
+    mainWin.geometry(f"550x{112 + (26 * numOfDevices)}")
 
     for i in range(0, numOfDevices):
         def toggleThis(index=i):
             smartHome.toggleSwitch(index)
+            updateWidgets()
+
+        def configThis(index=i):
+            configureDevice(smartHome.getDeviceAt(index))
             updateWidgets()
 
         txtDevice = Text(mainWin, height=1, width=50)
@@ -68,9 +112,13 @@ def setupWidgets():
         btnToggle = Button(mainWin, text="Toggle this", command=toggleThis)
         btnToggle.grid(row=(3 + i), column=1)
 
+        btnConfig = Button(mainWin, text="Configure", command=configThis)
+        btnConfig.grid(row=(3 + i), column=2)
+
     lblCount.grid(row=(4 + numOfDevices), column=0, padx=2, sticky="w")
 
 def main():
+    """Entry point for the application"""
     setupHome()
     setupWindow()
     setupWidgets()
